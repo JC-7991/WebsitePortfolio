e6bcf3b91d12456a6f1a1d47d6535b376b814577
JC <jctblock@gmail.com>
2023-01-04 11:58:17 -0500

    Checkpoint

--- DailyCoding/Week 11/task71.py
+++ DailyCoding/Week 11/task71.py
@@ -1,1 +1,3 @@
-# text
+# Using a function rand7() that returns an integer 
+# from 1 to 7 (inclusive) with uniform probability, 
+# implement a function rand5() that returns an integer from 1 to 5 (inclusive).
