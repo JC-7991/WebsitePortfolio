3af7121b2fc9b437079c2a0735e5f94d81a22f0a
JC <jctblock@gmail.com>
2023-01-06 00:01:35 -0500

    Checkpoint

--- DailyCoding/Week 11/task72.py
+++ DailyCoding/Week 11/task72.py
@@ -32,7 +32,7 @@ class GraphPath:
         return "nodes={}, letters={}".format(self.nodes, self.letter_counts)
 
 
-def strHelper(graph_path: GraphPath, node: str, adjacency_map: Dict[str, Set[str]]) -> List[GraphPath]:
+def strHelper(graph_path: GraphPath, node: str, adj_map: Dict[str, Set[str]]) -> List[GraphPath]:
 
     if node in graph_path.nodes:
         return [graph_path]
@@ -48,13 +48,13 @@ def strHelper(graph_path: GraphPath, node: str, adjacency_map: Dict[str, Set[str
 
     new_graph_path = GraphPath(new_nodes, new_letter_counts)
 
-    if node not in adjacency_map:
+    if node not in adj_map:
         return [new_graph_path]
 
     paths = list()
 
-    for child_node in adjacency_map[node]:
-        new_paths = strHelper(new_graph_path, child_node, adjacency_map)
+    for child_node in adj_map[node]:
+        new_paths = strHelper(new_graph_path, child_node, adj_map)
         paths.extend(new_paths)
 
     return paths
@@ -74,21 +74,21 @@ def maxString(graph_string: str, edge_list: List[Tuple[int, int]]) -> Optional[i
 
         nodes.append("{}{}".format(char, letter_counts[char]))
 
-    adjacency_map = dict()
+    adj_map = dict()
 
     for start, end in edge_list:
 
-        if nodes[start] not in adjacency_map:
-            adjacency_map[nodes[start]] = set()
+        if nodes[start] not in adj_map:
+            adj_map[nodes[start]] = set()
 
         if nodes[start] != nodes[end]:
-            adjacency_map[nodes[start]].add(nodes[end])
+            adj_map[nodes[start]].add(nodes[end])
 
     paths = list()
     graph_path = GraphPath()
 
-    for node in adjacency_map:
-        new_paths = strHelper(graph_path, node, adjacency_map)
+    for node in adj_map:
+        new_paths = strHelper(graph_path, node, adj_map)
         paths.extend(new_paths)
 
     maxValue = 0
