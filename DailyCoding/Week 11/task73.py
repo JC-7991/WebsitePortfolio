c083ec2f9869a83fc79827538c12db21c423b467
JC <jctblock@gmail.com>
2023-01-06 23:14:31 -0500

    Checkpoint

--- DailyCoding/Week 11/task73.py
+++ DailyCoding/Week 11/task73.py
@@ -1,6 +1,7 @@
 # Given the head of a singly linked list, reverse it in-place.
 
 class ListNode:
+
     def __init__(self, x):
         self.val = x
         self.next = None
@@ -8,3 +9,5 @@ class ListNode:
 class Solution:
 
     def reverseList(self, head: ListNode) -> ListNode:
+        if not head:
+            return
