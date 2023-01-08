ae44b5de21b42cd2cec1150d76fa53c149ebe159
JC <jctblock@gmail.com>
2023-01-08 12:07:12 -0500

    Checkpoint

--- DailyCoding/Week 11/task75.py
+++ DailyCoding/Week 11/task75.py
@@ -6,3 +6,38 @@ For example, given the array [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 
 the longest increasing subsequence has length 6: it is 0, 2, 6, 9, 11, 15.
 '''
 
+global maximum
+ 
+def lengthX(arr, n):
+
+    global maximum
+ 
+    if n == 1:
+        return 1
+
+    maxEndingHere = 1
+ 
+    for i in range(1, n):
+
+        res = lengthX(arr, i)
+        if arr[i - 1] < arr[n - 1] and res+1 > maxEndingHere:
+            maxEndingHere = res + 1
+ 
+    maximum = max(maximum, maxEndingHere)
+ 
+    return maxEndingHere
+ 
+ 
+def lengthY(arr):
+ 
+    global maximum
+    n = len(arr)
+    maximum = 1
+    lengthX(arr, n)
+ 
+    return maximum
+ 
+if __name__ == "__main__":
+    arr = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
+    n = len(arr)
+    print ("Length:", lengthY(arr))
