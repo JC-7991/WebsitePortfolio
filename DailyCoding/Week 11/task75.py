54de344d5b68f16fcb59d1779fd06db92c3c8da9
JC <jctblock@gmail.com>
2023-01-08 12:07:39 -0500

    Checkpoint

--- DailyCoding/Week 11/task75.py
+++ DailyCoding/Week 11/task75.py
@@ -32,12 +32,13 @@ def lengthY(arr):
     global maximum
     n = len(arr)
     maximum = 1
-    
+
     lengthX(arr, n)
  
     return maximum
  
 if __name__ == "__main__":
+    
     arr = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
     n = len(arr)
     print ("Length:", lengthY(arr))
