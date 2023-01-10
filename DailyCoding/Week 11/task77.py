f87053211383487243709ed33ce7c0da9daa3fb3
JC <jctblock@gmail.com>
2023-01-10 15:03:49 -0500

    Checkpoint

--- DailyCoding/Week 11/task77.py
+++ DailyCoding/Week 11/task77.py
@@ -11,6 +11,7 @@ def mergeIntervals(intervals):
     stack.append(intervals[0])
 
     for i in intervals[1:]:
+        
         if stack[-1][0] <= i[0] <= stack[-1][-1]:
             stack[-1][-1] = max(stack[-1][-1], i[-1])
         else:
