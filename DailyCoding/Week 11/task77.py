d95db112b96a531166ad81b771380c75a7dd7024
JC <jctblock@gmail.com>
2023-01-10 15:02:34 -0500

    Checkpoint

--- DailyCoding/Week 11/task77.py
+++ DailyCoding/Week 11/task77.py
@@ -3,3 +3,24 @@ Given a list of possibly overlapping intervals, return a new list of intervals w
 The input list is not necessarily ordered in any way.
 For example, given [(1, 3), (5, 8), (4, 10), (20, 25)], you should return [(1, 3), (4, 10), (20, 25)].
 '''
+
+def mergeIntervals(intervals):
+
+    intervals.sort()
+    stack = []
+    stack.append(intervals[0])
+
+    for i in intervals[1:]:
+        if stack[-1][0] <= i[0] <= stack[-1][-1]:
+            stack[-1][-1] = max(stack[-1][-1], i[-1])
+        else:
+            stack.append(i)
+ 
+    print("The Merged Intervals are: ", end = " ")
+
+    for i in range(len(stack)):
+        print(stack[i], end=" ")
+
+if __name__ == "__main__":
+    arr = [[6, 8], [1, 9], [2, 4], [4, 7]]
+    mergeIntervals(arr)
