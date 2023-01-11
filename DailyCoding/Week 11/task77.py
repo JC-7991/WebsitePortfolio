42cccd9dfa54b88e0a4209c62c11c23a91d1da7d
JC <jctblock@gmail.com>
2023-01-11 00:03:37 -0500

    Checkpoint

--- DailyCoding/Week 11/task77.py
+++ DailyCoding/Week 11/task77.py
@@ -4,20 +4,20 @@ The input list is not necessarily ordered in any way.
 For example, given [(1, 3), (5, 8), (4, 10), (20, 25)], you should return [(1, 3), (4, 10), (20, 25)].
 '''
 
-def mergeInv(intervals):
+def mergeInv(ints):
 
-    intervals.sort()
+    ints.sort()
     stack = []
-    stack.append(intervals[0])
+    stack.append(ints[0])
 
-    for i in intervals[1:]:
+    for i in ints[1:]:
 
         if stack[-1][0] <= i[0] <= stack[-1][-1]:
             stack[-1][-1] = max(stack[-1][-1], i[-1])
         else:
             stack.append(i)
  
-    print("Merged intervals:", end = " ")
+    print("Merged ints:", end = " ")
 
     for i in range(len(stack)):
         print(stack[i], end = " ")
