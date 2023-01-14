05afbeaa7b4da5640b7631e373d8314a9573ac2a
JC <jctblock@gmail.com>
2023-01-13 21:42:16 -0500

    Checkpoint

--- DailyCoding/Week 12/task80.py
+++ DailyCoding/Week 12/task80.py
@@ -17,32 +17,23 @@ class newNode:
 
 def find(root, level, maxLevel, res):
  
-    if (root != None):
+    if(root != None):
+
         level += 1
         find(root.left, level, maxLevel, res)
- 
-        # Update level and rescue
         if (level > maxLevel[0]):
-         
             res[0] = root.data
             maxLevel[0] = level
          
         find(root.right, level, maxLevel, res)
-         
-# Returns value of deepest node
+
 def deepestNode(root) :
  
-    # Initialize result and max level
     res = [-1]
     maxLevel = [-1]
- 
-    # Updates value "res" and "maxLevel"
-    # Note that res and maxLen are passed
-    # by reference.
     find(root, 0, maxLevel, res)
     return res[0]
                          
-# Driver Code
 if __name__ == '__main__':
     root = newNode(1)
     root.left = newNode(2)
