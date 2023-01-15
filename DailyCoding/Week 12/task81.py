7563e0d80582e3bbca8081136852682e57f08c64
JC <jctblock@gmail.com>
2023-01-15 16:53:21 -0500

    Checkpoint

--- DailyCoding/Week 12/task81.py
+++ DailyCoding/Week 12/task81.py
@@ -6,3 +6,31 @@ For example if {“2”: [“a”, “b”, “c”], 3: [“d”, “e”, “f
 should return [“ad”, “ae”, “af”, “bd”, “be”, “bf”, “cd”, “ce”, “cf"].
 '''
 
+from typing import Dict, List
+
+def mappings(convert: Dict[str, str], string: str, result: List[str] = []) -> List[str]:
+
+    if not string:
+        return result
+
+    if not result:
+
+        for elem in convert[string[0]]:
+            result.append(elem)
+        return mappings(convert, string[1:], result)
+
+    temp = []
+
+    for part in result:
+        for elem in convert[string[0]]:
+            temp.append(part + elem)
+    
+    result[:] = temp
+    
+    return mappings(convert, string[1:], result)
+
+if __name__ == "__main__":
+
+    print(mappings({"2": ["a", "b", "c"], "3": ["d", "e", "f"]}, "23", []))
+    print(mappings({"2": ["a", "b", "c"], "3": ["d", "e", "f"]}, "32", []))
+    print(mappings({"2": ["a", "b", "c"], "3": ["d", "e", "f"]}, "222", []))
