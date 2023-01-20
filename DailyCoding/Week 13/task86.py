d51fa8d5b8b59be42e9f885ae6fcbb34df852368
JC <jctblock@gmail.com>
2023-01-20 14:12:39 -0500

    Checkpoint

--- DailyCoding/Week 13/task86.py
+++ DailyCoding/Week 13/task86.py
@@ -5,3 +5,20 @@ removed to make the string valid (i.e. each open parenthesis is eventually close
 For example, given the string "()())()", you should return 1. 
 Given the string ")(", you should return 2, since we must remove all of them.
 '''
+
+class Solution:
+
+    def validRemove(self, S: str) -> str:
+
+        S, stack = list(S), []
+
+        for i, c in enumerate(S):
+
+            if c == ")":
+                if stack: stack.pop()
+                else: S[i] = ""
+
+            elif c == "(": stack.append(i)
+            
+        for i in stack: S[i] = ""
+        return "".join(S)
