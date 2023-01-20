9546c811113b6793c6696f8c199f120ee2f18722
JC <jctblock@gmail.com>
2023-01-19 22:01:36 -0500

    Checkpoint

--- DailyCoding/Week 13/task85.py
+++ DailyCoding/Week 13/task85.py
@@ -5,9 +5,9 @@ mathematical or bit operations. You can assume b
 can only be 1 or 0.
 '''
 
-def swicth(x, y, b):
+def switch(x, y, b):
     return (x * b) + (y * abs(b - 1))
 
 if __name__ == "__main__":
-    print(swicth(6, 8, 1))
-    print(swicth(6, 8, 0))
+    print(switch(6, 8, 1))
+    print(switch(6, 8, 0))
