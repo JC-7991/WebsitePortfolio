49369e7c0829e72eaa35c0e58d67ab432912b09c
JC <jctblock@gmail.com>
2023-01-22 00:02:04 -0500

    Checkpoint

--- DailyCoding/Week 13/task88.py
+++ DailyCoding/Week 13/task88.py
@@ -3,3 +3,17 @@ Implement division of two positive integers without using the division, multipli
 or modulus operators. Return the quotient as an integer, ignoring the remainder.
 '''
 
+def divide(dividend: int, divisor: int) -> int:
+
+    quo = 0
+
+    while dividend > 0:
+        dividend -= divisor
+        if dividend >= 0:
+            quo += 1
+
+    return quo
+
+if __name__ == "__main__":
+    print(divide(1, 0))
+
