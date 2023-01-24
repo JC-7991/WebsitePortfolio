f4716b4d9b710e5365112f2dd54efe47e76ea625
JC <jctblock@gmail.com>
2023-01-24 17:22:34 -0500

    Checkpoint

--- DailyCoding/Week 13/task90.py
+++ DailyCoding/Week 13/task90.py
@@ -4,3 +4,23 @@ function that randomly generates a number from 0 to
 n-1 that isn't in l (uniform).
 '''
 
+import random
+
+def generateRandom(l, n):
+
+	nums = []
+
+	for i in range(1,n + 1):
+		if i not in l:
+			nums.append(i)
+
+	return nums[random.randint(0, len(nums))]
+	
+if __name__ == "__main__":
+
+    l = [2, 3, 4, 7, 8, 14, 18]
+    n = 25
+
+    print(generateRandom(l, n))
+    print(generateRandom(l, n))
+    print(generateRandom(l, n))
