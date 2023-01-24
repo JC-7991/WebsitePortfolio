6a9e3dd58b535bfd45901e6127fecbb8eba29e33
JC <jctblock@gmail.com>
2023-01-23 20:43:26 -0500

    Checkpoint

--- DailyCoding/Week 13/task89.py
+++ DailyCoding/Week 13/task89.py
@@ -15,12 +15,12 @@ class Node:
  
 def maxValue(node):
     if node is None:
-        return 0;
+        return 0
      
     leftMax = maxValue(node.left)
     rightMax = maxValue(node.right)
      
-    value = 0;
+    value = 0
     if leftMax > rightMax:
         value = leftMax
     else:
@@ -69,17 +69,17 @@ def isBST(node):
     # passing all that, it's a BST
     return True
  
-# Driver code
 if __name__ == "__main__":
-  root = Node(4)
-  root.left = Node(2)
-  root.right = Node(5)
-  # root.right.left = Node(7)
-  root.left.left = Node(1)
-  root.left.right = Node(3)
- 
-  # Function call
-  if isBST(root) is True:
-      print("Is BST")
-  else:
-      print("Not a BST")
+    
+    root = Node(4)
+    root.left = Node(2)
+    root.right = Node(5)
+    # root.right.left = Node(7)
+    root.left.left = Node(1)
+    root.left.right = Node(3)
+
+    # Function call
+    if isBST(root) is True:
+        print("Is BST")
+    else:
+        print("Not a BST")
