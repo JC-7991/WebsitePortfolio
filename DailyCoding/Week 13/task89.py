711c98c41eafb01304658c0fd27f2c639d597db0
JC <jctblock@gmail.com>
2023-01-23 20:43:59 -0500

    Checkpoint

--- DailyCoding/Week 13/task89.py
+++ DailyCoding/Week 13/task89.py
@@ -14,6 +14,7 @@ class Node:
         self.right = None
  
 def maxValue(node):
+    
     if node is None:
         return 0
      
@@ -32,6 +33,7 @@ def maxValue(node):
     return value
      
 def minValue(node):
+
     if node is None:
         return 1000000000
      
@@ -48,37 +50,31 @@ def minValue(node):
         value = node.data
      
     return value
- 
-# Returns true if a binary tree is a binary search tree
+
 def isBST(node):
+
     if node is None:
         return True
-     
-    # false if the max of the left is > than us
+
     if(node.left is not None and maxValue(node.left) > node.data):
         return False
-     
-    # false if the min of the right is <= than us
+
     if(node.right is not None and minValue(node.right) < node.data):
         return False
-     
-    #false if, recursively, the left or right is not a BST
+
     if(isBST(node.left) is False or isBST(node.right) is False):
         return False
      
-    # passing all that, it's a BST
     return True
  
 if __name__ == "__main__":
-    
+
     root = Node(4)
     root.left = Node(2)
     root.right = Node(5)
-    # root.right.left = Node(7)
     root.left.left = Node(1)
     root.left.right = Node(3)
 
-    # Function call
     if isBST(root) is True:
         print("Is BST")
     else:
