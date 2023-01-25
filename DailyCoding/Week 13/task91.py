42e629f3e66f193a176c4454cc3e305dda6a2266
JC <jctblock@gmail.com>
2023-01-25 12:46:51 -0500

    Checkpoint

--- DailyCoding/Week 13/task91.py
+++ DailyCoding/Week 13/task91.py
@@ -9,10 +9,14 @@ for f in functions:
     print(f())
 '''
 if __name__ == "__main__":
+
     functions = []
+
     for i in range(10):
         functions.append(lambda i: i)
+
     i = 0
+    
     for f in functions:
         print(f(i))
         i += 1
