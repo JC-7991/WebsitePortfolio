4967d545822bdf7c259314b95b0218857413e235
JC <jctblock@gmail.com>
2023-01-29 00:01:38 -0500

    Checkpoint

--- DailyCoding/Week 14/task95.py
+++ DailyCoding/Week 14/task95.py
@@ -6,3 +6,38 @@ For example, the list [1,2,3] should return [1,3,2]. The list [1,3,2] should ret
 [2,1,3]. The list [3,2,1] should return [1,2,3]
 '''
 
+from typing import List
+
+def get_next(arr: List[int]) -> List[int]:
+
+    length = len(arr)
+    if length < 2:
+        return arr
+
+    for index in range(length - 1, -1, -1):
+        if index > 0 and arr[index - 1] < arr[index]:
+            break
+
+    if index == 0:
+        arr.reverse()
+        return arr
+
+    for k in range(length - 1, index - 1, -1):
+        if arr[k] > arr[index - 1]:
+            arr[k], arr[index - 1] = arr[index - 1], arr[k]
+            break
+
+    size = (length - 1) + index
+    for i in range(index, (size + 1) // 2):
+        arr[i], arr[size - i] = arr[size - i], arr[i]
+    return arr
+
+
+if __name__ == "__main__":
+
+    print(get_next([1, 2, 3]))
+    print(get_next([1, 3, 2]))
+    print(get_next([2, 1, 3]))
+    print(get_next([2, 3, 1]))
+    print(get_next([3, 1, 2]))
+    print(get_next([3, 2, 1]))
