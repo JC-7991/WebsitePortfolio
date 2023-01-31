850ed6defdb32b238fc065e1fe619ee2ae39acda
JC <jctblock@gmail.com>
2023-01-31 13:14:15 -0500

    Checkpoint

--- DailyCoding/Week 14/task97.py
+++ DailyCoding/Week 14/task97.py
@@ -1,11 +1,15 @@
 '''
-Write a map implementation with a get function that lets you retrieve the value of a key at a particular time.
+Write a map implementation with a get function that lets you 
+retrieve the value of a key at a particular time.
 It should contain the following methods:
 
 set(key, value, time): sets key to value for t = time.
 get(key, time): gets the key at t = time.
-The map should work like this. If we set a key at a particular time, it will maintain that value forever or until it gets set at a later time. In other words, when we get a key at a time, it should return the value that was set for that key set at the most recent time.
 
+The map should work like this. If we set a key at a particular time, 
+it will maintain that value forever or until it gets set at a later time. 
+In other words, when we get a key at a time, it should return the value 
+that was set for that key set at the most recent time.
 Consider the following examples:
 
 d.set(1, 1, 0) # set key 1 to value 1 at time 0
