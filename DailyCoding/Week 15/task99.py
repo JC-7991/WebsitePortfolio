3c3f5c89d9faf5a4f0806b2c20d9624028e64432
JC <jctblock@gmail.com>
2023-02-05 21:52:22 -0500

    Checkpoint

---
+++ DailyCoding/Week 15/task99.py
@@ -0,0 +1,38 @@
+'''
+Given an unsorted array of integers, find the length of the longest consecutive
+elements sequence.
+For example, given [100, 4, 200, 1, 3, 2], the longest consecutive element sequence is
+[1, 2, 3, 4]. Return its length: 4.
+Your algorithm should run in O(n) complexity.
+'''
+
+def findLongest(arr, n):
+
+    ans = 0
+    cnt = 0
+    arr.sort()
+
+    x = []
+    x.append(arr[0])
+
+    # Repeated elements are only inserted once into the vector.
+    for i in range(1, n):
+        if(arr[i] != arr[i - 1]):
+            x.append(arr[i])
+
+    for i in range(len(x)):
+
+        if(i > 0 and x[i] == x[i - 1]):
+            cnt += 1
+        else:
+            cnt = 1
+        ans = max(ans, cnt)
+
+    return ans
+
+if __name__ == "__main__":
+
+    arr = [100, 4, 200, 1, 3, 2]
+    n = len(arr)
+    print("Length of the Longest contiguous subsequence is", findLongest(arr, n))
+ 
