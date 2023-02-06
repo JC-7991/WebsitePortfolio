0f1db061e13d2a52803c6181ce1bad4b0606e638
JC <jctblock@gmail.com>
2023-02-06 00:05:13 -0500

    Checkpoint

--- DailyCoding/Week 15/task99.py
+++ DailyCoding/Week 15/task99.py
@@ -17,6 +17,7 @@ def findLongest(arr, n):
 
     # Repeated elements are only inserted once into the vector.
     for i in range(1, n):
+        
         if(arr[i] != arr[i - 1]):
             x.append(arr[i])
 
