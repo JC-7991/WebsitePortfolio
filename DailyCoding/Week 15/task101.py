d3c3f651bde4061c300b9a5792fa1b097bb9d84a
JC <jctblock@gmail.com>
2023-02-07 15:30:11 -0500

    Checkpoint

--- DailyCoding/Week 15/task101.py
+++ DailyCoding/Week 15/task101.py
@@ -3,7 +3,9 @@ Given an even number (greater than 2), return two prime numbers whose sum will b
 to the given number.
 A solution will always exist. See Goldbach's conjecture.
 Example:
-numut: 4 Output: 2 + 2 = 4 If there are more than one solution possible, return the
+Input: 4
+Output: 2 + 2 = 4 
+If there are more than one solution possible, return the
 lexicographically smaller solution.
 If [a, b] is one solution with a <= b, and [c, d] is another solution with c <= d, then
 [a, b] < [c, d]
