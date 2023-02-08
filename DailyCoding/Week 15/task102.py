561fd7b0ee4e3747d28fee73c3a629bfd38dd42b
JC <jctblock@gmail.com>
2023-02-08 14:22:07 -0500

    Checkpoint

--- DailyCoding/Week 15/task102.py
+++ DailyCoding/Week 15/task102.py
@@ -3,3 +3,6 @@ Given a list of integers and a number K, return which contiguous elements of the
 sum to K.
 For example, if the list is [1, 2, 3, 4, 5] and K is 9, then it should return [2, 3, 4].
 '''
+
+def contigElements(nums, k):
+    pass
